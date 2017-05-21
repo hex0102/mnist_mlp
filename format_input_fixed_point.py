34a98095b0791f051ac78f857e24e138b4a92179
hex0102 <hex0102@gmail.com>
2017-05-20 21:43:01 -0500

    inference nonideal v0.1

--- format_input_fixed_point.py
+++ format_input_fixed_point.py
@@ -4,13 +4,16 @@ from torch.autograd import Variable
 
 class Formatting(torch.autograd.Function):
 
-    def foward(self, x, format, IL,FL):
-        x = apply_format(format,x,IL,FL);
+    def forward(self, x):
+        format='FXP'
+        IL=4
+        FL=12
+        x = apply_format(format,x,IL,FL)
         return x
 
 
 
-    def backward(self, *grad_output):
+    def backward(self, grad_output):
         grad_input = grad_output.clone()
         return grad_input
 
@@ -23,6 +26,14 @@ def apply_format(format, X, IL, FL):
 
 
 def fixed_point(x, IL, FL):
+    power = 2. ** FL
+    MAX = (2.**(IL+FL))-1
+    x.mul_(power)
+    x.round_()
+    x.clamp_(min=-MAX, max=MAX)
+    x.div_(power)
+    #x = torch.clamp(x,min=-MAX, max=MAX)
+    #x = torch.div(x,power)
     return x
 
 def overflow(vector, IL, FL):
--- mlp_mnist.py
+++ mlp_mnist.py
@@ -6,14 +6,18 @@ import torchvision
 #import matplotlib.
 #hexin  hexin
 from format_input_fixed_point import Formatting
+from format_input_fixed_point import apply_format
 
 torch.manual_seed(1)
 
 
-EPOCH = 10
+EPOCH = 2
 BATCH_SIZE = 50
 LR = 0.01
 DOWNLOAD_MNIST = True
+IL = torch.IntTensor([4])
+FL = torch.IntTensor([12])
+config = torch.IntTensor([0])
 
 train_data = torchvision.datasets.MNIST(
     root = './mnist/',
@@ -29,7 +33,8 @@ train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffl
 test_x = Variable(torch.unsqueeze(test_data.test_data, dim=1),volatile=True).type(torch.FloatTensor)[:2000]/255.
 test_y = test_data.test_labels[:2000]
 
-print(test_y[1])
+
+
 
 class MLP(nn.Module):
     def __init__(self):
@@ -52,10 +57,11 @@ class MLP(nn.Module):
         self.activation = nn.Sigmoid()
         self.out = nn.Linear(100,10)
 
-    def forward(self,x,):
+    def forward(self,x, config):
         x = x.view(x.size(0),-1)
         x = self.hidden1(x)
-        x = self.formatting(x,'FXP',12,24)
+        if(torch.equal(config, torch.IntTensor([1]))):
+            x = self.formatting(x)
         x = self.activation(x)
         output = self.out(x)
         return output #print(x)
@@ -71,22 +77,41 @@ for epoch in range(EPOCH):
         b_x = Variable(x)
         b_y = Variable(y)
 
-        output = mlp(b_x)
+        output = mlp(b_x,config)
         loss = loss_func(output,b_y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         if step % 50 == 0:
-            #print('Epoch: ',epoch,'| train loss : %.4f' %loss.data[0])
-            
-            test_output = mlp(test_x)
+            test_output = mlp(test_x,config)
             pred_y = torch.max(test_output,1)[1].data.squeeze()
             accuracy = sum(pred_y == test_y)/float(test_y.size(0))
             print('Epoch: ',epoch,'| train loss : %.4f' %loss.data[0], '| test accuracy: %.2f' % accuracy)
             
 
-test_output = mlp(test_x[:10])
+
+hidden = list(mlp.hidden1.parameters())
+out = list(mlp.out.parameters())
+
+#print(linear1)   parameter -> contain both weight and bias
+#print(linear1[0]) parameter -> contain weight
+#print(linear1[0].data)   weight tensor
+
+
+
+
+#apply_format('FXP',linear1[0].data,4,12)
+#apply_format('FXP',linear1[0],4,12)
+
+
+
+#Test neural network performance
+config = torch.IntTensor([1])
+
+test_output = mlp(test_x[:10],config)
 pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
 print(pred_y, 'prediction number')
 print(test_y[:10].numpy(), 'real number')
+
+print('End of testing!')
