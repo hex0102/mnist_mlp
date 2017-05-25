fix some bugs on gradients formulation
Backhook returns modified gradient
forwardhook directly change it inside

mlp_mnist.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 import torch.nn as nn
+import torch.nn.init as init
 from torch.autograd import Variable
 import torch.utils.data as Data
 from mlp_class import MLP
@@ -11,7 +12,7 @@ from utils import *
 torch.manual_seed(1)
 
 
-EPOCH = 0
+EPOCH = 30
 BATCH_SIZE = 100
 LR = 0.01
 DOWNLOAD_MNIST = True
@@ -19,8 +20,8 @@ save_model = 1
 
 
 IL = 4  # IL = torch.IntTensor([4])
-FL = 10 # FL = torch.IntTensor([12])
-nonideal_train = 0
+FL = 14 # FL = torch.IntTensor([12])
+nonideal_train = 1
 nonideal_inference = 1
 
 train_data = torchvision.datasets.MNIST(
@@ -45,14 +46,24 @@ def fixed_point_hook(self, input, output):
 
 def fixed_point_back_hook(self, grad_in, grad_out):
     #print('Inside ' + self.__class__.__name__ + ' backward')
-    if grad_in[0] is not None:
-        apply_format_inplace('FXP', grad_in[0].data, IL, FL)
+    #print('grad_input size:', grad_in[0].size())
+    #print('grad_input size:', grad_in[1].size())
+    #print('grad_input size:', grad_in[2].size())
+    #print('grad_input tuple size: ', grad_in.__len__())
+    if grad_in.__len__() > 1:
+        #print('grad_input size:', grad_in[1].size())
+        apply_format_inplace('FXP', grad_in[1].data, IL, FL)
+        apply_format_inplace('FXP', grad_in[2].data, IL, FL)
+        #print(type(grad_in[0]))
+        #print(type(grad_in[1]))
+        #print(type(grad_in[2]))
+        return grad_in
 
 
 
 
 mlp=MLP()
-optimizer = torch.optim.SGD(mlp.parameters(), lr=LR, momentum=0.9)
+optimizer = torch.optim.SGD(mlp.parameters(), lr=LR)#, momentum=0.9)
 loss_func = nn.CrossEntropyLoss()
 
 #add backward hooker on each layer, to regulate the gradient to fixed point representation
@@ -68,11 +79,13 @@ if(nonideal_train):
 
 #>>>>>>>>Dump neural network weight to list
 linear = list(mlp.hidden1.parameters())
+init.constant(linear[1].data,0)
 out = list(mlp.out.parameters())
+init.constant(out[1].data,0)
 
 for epoch in range(EPOCH):
     for step, (x,y) in enumerate(train_loader):
-        b_x = Variable(x)
+        b_x = Variable(x,requires_grad = True)
         b_y = Variable(y)
 
         output = mlp(b_x)
@@ -82,8 +95,10 @@ for epoch in range(EPOCH):
         optimizer.step()
 
         if(nonideal_train):
-            apply_format_inplace('FXP', linear[0].data, IL, FL)
+            apply_format_inplace('FXP', linear[0].data, IL, FL)#weight
+            apply_format_inplace('FXP', linear[1].data, IL, FL)#bias
             apply_format_inplace('FXP', out[0].data, IL, FL)
+            apply_format_inplace('FXP', out[1].data, IL, FL)
 
         if step % 50 == 0:
             print('Epoch: ', epoch, '| train loss : %.4f' % loss.data[0])
@@ -104,9 +119,11 @@ if save_model:
 #>>>>>>>>Regulate neural network weight into fixed point counterpart
 if nonideal_inference:
     apply_format_inplace('FXP',linear[0].data,IL,FL) # weight regulation
-    apply_format_inplace('FXP',out[0].data,IL,FL)    # bias regulation
+    apply_format_inplace('FXP', linear[1].data, IL, FL)
+    apply_format_inplace('FXP',out[0].data,IL,FL)    # weight regulation
+    apply_format_inplace('FXP', out[1].data, IL, FL)
     apply_format_inplace('FXP',test_x.data,IL,FL)    # input regulation
-    apply_bitflip(linear[0].data,0.5,3,10,13)
+    #apply_bitflip(linear[0].data,0.5,3,10,13)
 
 #print(type(test_x)) #<class 'torch.autograd.variable.Variable'>
 #print(type(test_x.data)) #<class 'torch.FloatTensor'>