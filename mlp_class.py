3ecbe831993057cff503b80ba5a595f4e049893a
hex0102 <hex0102@gmail.com>
2017-08-14 11:14:07 -0500

    add second backward for fms

--- mlp_class.py
+++ mlp_class.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 class MLP(nn.Module):
     def __init__(self):
         super(MLP,self).__init__()
-        self.hidden1 = nn.Linear(784,1000)
+        self.hidden1 = nn.Linear(784,500)
         #self.formatting = Formatting()
         self.activation = nn.Sigmoid()
-        self.hidden2 = nn.Linear(1000, 10)
+        self.hidden2 = nn.Linear(500, 10)
         self.activation2 = nn.Sigmoid()
         #self.activation = nn.Sigmoid()
         self.out = nn.Linear(1000,10)
